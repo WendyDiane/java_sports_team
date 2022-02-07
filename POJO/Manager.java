01399a89c5adbf46972d405cfb110d06767f2c3a
Wendy Hamenya <wendyhamenya@yahoo.co.uk>
2022-02-07 11:57:35 +0000

    add manager class

--- POJO/Manager.java
+++ POJO/Manager.java
@@ -1,5 +1,44 @@
 package com.bnta.wendy.Rugby.POJO;
 
+import java.util.Objects;
+
 public class Manager {
     private String name;
+
+    //Constructor
+
+    public Manager(){
+
+    }
+
+    //Getter and Setter
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Manager{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Manager manager = (Manager) o;
+        return Objects.equals(name, manager.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
