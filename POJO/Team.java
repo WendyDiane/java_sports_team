d358983b35235c0c55fb9d422f4248b1216211ab
Wendy Hamenya <wendyhamenya@yahoo.co.uk>
2022-02-07 12:20:33 +0000

    add team Class

--- POJO/Team.java
+++ POJO/Team.java
@@ -5,6 +5,39 @@ import com.bnta.wendy.Rugby.POJO.Player;
 public class Team {
     private Player[] players;
     private String name;
-    com.bnta.wendy.Rugby.POJO.Manager Manager;
+    private Manager Manager;
 
+    public Team(){
+
+    }
+
+    public Team(String name, Manager manager){
+        this.name = name;
+        this.Manager = manager;
+        this.players = new Player[15];
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public Manager getManager() {
+        return Manager;
+    }
+
+    public void setManager(Manager manager) {
+        Manager = manager;
+    }
+
+    public Player[] getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(Player[] players) {
+        this.players = players;
+    }
 }
